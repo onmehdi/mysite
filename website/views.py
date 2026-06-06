c2eeed9545e30bf4dbe86c9a51e36b7897c163c3
Mehdi Ahmadi <60336119+onmehdi@users.noreply.github.com>
2026-06-05 22:42:29 -0700

    create render page
    create render page in python

--- website/views.py
+++ website/views.py
@@ -9,10 +9,10 @@ def json_test(request):
     return JsonResponse({'name':'ali'})
 
 def index_view(request):
-    return HttpResponse("<h1>home page</h1>")
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return HttpResponse("<h1>about page</h1>")
+    return render(request,'about.html')
 
 def contact_view(request):
-    return HttpResponse("<h1>contact page</h1>")
+    return render(request,'contact.html')
